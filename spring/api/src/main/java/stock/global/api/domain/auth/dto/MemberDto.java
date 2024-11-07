e5c1ace44d97c572155d2a6f4649d7a1eb230ae2
crocus-jjh <jjh930301@crocusenergy.com>
2024-11-07 13:35:39 +0900

    :test_tube: : test case

--- spring/api/src/main/java/stock/global/api/domain/auth/dto/MemberDto.java
+++ spring/api/src/main/java/stock/global/api/domain/auth/dto/MemberDto.java
@@ -3,7 +3,12 @@ package stock.global.api.domain.auth.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
+@Setter
+@NoArgsConstructor
 public class MemberDto {
     @Schema
     @Size(min = 8 , message = "id min length is 8")
@@ -12,4 +17,12 @@ public class MemberDto {
     @Schema
     @Size(min = 8 , message = "password min length is 8")
     private String accountPassword;
+
+    public MemberDto(
+        String accountId,
+        String accountPassword
+    ) {
+        this.setAccountId(accountId);
+        this.setAccountPassword(accountPassword);
+    }
 }
